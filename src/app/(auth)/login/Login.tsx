14f2bfbc059e793ef601eff6113ce3c1fb03fbab
qthanggg <quocthang2024@gmail.com>
2024-05-22 21:16:06 +0700

    fix ui

---
+++ src/app/(auth)/login/Login.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React, { useState } from 'react'
+
+export default function Login() {
+const [first, setfirst] = useState();
+    return (
+    <div>Login</div>
+  )
+}
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
+
+import Login from '@/app/(auth)/login/Login'
 import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
+    <div>
+      <main>
+        <Login/>
+      </main>
+    </div>
   )
 }
